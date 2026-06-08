a9eac7ae7e76fc840bf8d38d8ec8d93f2f478aad
Sania Saji <saniasaji095@gmail.com>
2026-06-08 20:21:54 +0530

    Updated homepage from feature branch

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
 from flask import Flask
-from organizer import organize_files
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def home():
+    return "Smart Organizer is Live 🚀"
 
-    organize_files("uploads")
-
-    return "Files Organized Successfully 😁🔥"
-
-if __name__ == '__main__':
-
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
